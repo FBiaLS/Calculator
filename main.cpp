4626b36f09fd97c8437bfdd19d65ffa318a4c0cc
FBiaLS <fprockter9@gmail.com>
2023-11-13 21:17:43 +0000

    ij

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-using namespace std
+using namespace std;
 
 string add(double number, double number2, double ans){ //defines function add as string containing listed varaibles
   ans = number + number2; //add number and number2
