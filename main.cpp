e4d1a06f3f31a4c51c885eab83dd5e2b4383779c
FBiaLS <fprockter9@gmail.com>
2023-11-14 20:50:43 +0000

    i

--- main.cpp
+++ main.cpp
@@ -2,27 +2,27 @@
 #include <cmath>
 using namespace std;
 
-string inp2(double number2){
+double inp2(double number2){
   cout << "Enter the second number for your calculation\n"; //print
   cin >> number2; //input for number2
 }
 
-string add(double number, double number2, double ans){ //defines function add as string containing listed varaibles
+double add(double number, double number2, double ans){ //defines function add as string containing listed varaibles
   ans = number + number2; //add number and number2
   cout << number << "+" << number2 << "=" << ans << "\n"; //print
 }
 
-string subtract(double number, double number2, double ans){ //defines function subtract as string containing listed varaibles
+double subtract(double number, double number2, double ans){ //defines function subtract as string containing listed varaibles
   ans = number - number2; //subtract number and number2
   cout << number << "-" << number2 << "=" << ans << "\n"; //print
 }
 
-string multiply(double number, double number2, double ans){ //defines function multiply as string containing listed varaibles
+double multiply(double number, double number2, double ans){ //defines function multiply as string containing listed varaibles
   ans = number * number2; //multiply number and number2
   cout << number << "x" << number2 << "=" << ans << "\n"; //print
 }
 
-string divide(double number, double number2, double ans){ //defines function divide as string containing listed varaibles
+double divide(double number, double number2, double ans){ //defines function divide as string containing listed varaibles
   ans = number / number2; //divide number and number2
   cout << number << "÷" << number2 << "=" << ans << "\n"; //print
 }
@@ -41,20 +41,24 @@ int main() {
   cin >> calc; //input for calc
 
   if (calc == "add" || calc == "plus" || calc == "+"){ //if calc is add
-    inp2(number2);
+    number2 = inp2(number2);
     add(number, number2, ans);
+    return number2;
   }
   else if (calc == "subtract" || calc == "minus" || calc == "-"){ //if calc is subtract
-    inp2(number2);
+    number2 = number2 = inp2(number2);
     subtract(number, number2, ans);
+    return number2;
   }
   else if (calc == "multiply" || calc == "times" || calc == "x"){ //if calc is multiply
-    inp2(number2);
+    number2 = inp2(number2);
     multiply(number, number2, ans);
+    return number2;
   }
   else if (calc == "divide" || calc == "/" || calc == "÷"){ //if calc is divide
-    inp2(number2);
+    number2 = inp2(number2);
     divide(number, number2, ans);
+    return number2;
   }
   else if (calc == "powers" || calc == "power" || calc == "use powers" || calc == "^"){
     cout << "What would you like to do " << number << " to the power of?\n"; //print
